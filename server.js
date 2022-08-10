0539896b72ae324c5fda4ba721b917a6803e4605
TheTrueZK <thetruezk@gmail.com>
2022-08-10 09:59:46 -0500

    added use requests

--- server.js
+++ server.js
@@ -4,5 +4,13 @@ const fs = require("fs");
 const database = require("./db/db.json");
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`server now on port ${PORT}!`)
+})
+
 
 
