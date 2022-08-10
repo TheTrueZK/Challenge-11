ea08fc6e7c085533ab99ad64852b7ad85d513886
TheTrueZK <thetruezk@gmail.com>
2022-08-10 14:07:05 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
