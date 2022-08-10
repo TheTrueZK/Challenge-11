a0a714819051578dfed35ee64e799dbb25809336
TheTrueZK <thetruezk@gmail.com>
2022-08-10 10:05:26 -0500

    added get requests

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+    res.json(database.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get ('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
 app.listen(PORT, () => {
     console.log(`server now on port ${PORT}!`)
 })
