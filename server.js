3c34da2937abbef9479680976dad2707003464bd
TheTrueZK <thetruezk@gmail.com>
2022-08-11 08:32:59 -0500

    linked index to server

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-index();
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
